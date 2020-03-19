78a20b4eec25e7ea44c083627c1c697e669f478a
Marc <m21.cerutti@gmail.com>
2020-03-19 17:04:17 +0100

    bad disk

--- im-proc/morphology/se.c
+++ im-proc/morphology/se.c
@@ -123,35 +123,44 @@ Diamond(pnm shape, int hs)
 void
 Disk(pnm shape, int hs)
 {
- //int size = 2*hs+1;
+  //int size = 2*hs+1;
 
-  int d, y, x;
- 
-  d = 3 - (2 * hs);
-  x = 0;
-  y = hs;
- 
-  while (y >= x) {
+ int rpart = hs;
 
-      for (int channel = 0; channel < 3; channel++) {
-    pnm_set_component(shape, hs + x, hs + y, channel, 255);
-    pnm_set_component(shape, hs + y, hs + x, channel, 255);
-    pnm_set_component(shape, hs - x, hs + y, channel, 255);
-    pnm_set_component(shape, hs - y, hs + x, channel, 255);
-    pnm_set_component(shape, hs + x, hs - y, channel, 255);
-    pnm_set_component(shape, hs + y, hs - x, channel, 255);
-    pnm_set_component(shape, hs - x, hs - y, channel, 255);
-    pnm_set_component(shape, hs - y, hs - x, channel, 255);
-      }
- 
-    if (d < 0)
-      d = d + (4 * x) + 6;
-    else {
-      d = d + 4 * (x - y) + 10;
-      y--;
-    }
-    x++;
+  while(rpart > 0)
+  {
+     int d, y, x;
+
+    d = 3 - (2 * rpart);
+    x = 0;
+    y = rpart;
+
+      while (y >= x) 
+          {
+              for (int channel = 0; channel < 3; channel++) 
+              {
+                pnm_set_component(shape, hs + x, hs + y, channel, 255);
+                pnm_set_component(shape, hs + y, hs + x, channel, 255);
+                pnm_set_component(shape, hs - x, hs + y, channel, 255);
+                pnm_set_component(shape, hs - y, hs + x, channel, 255);
+                pnm_set_component(shape, hs + x, hs - y, channel, 255);
+                pnm_set_component(shape, hs + y, hs - x, channel, 255);
+                pnm_set_component(shape, hs - x, hs - y, channel, 255);
+                pnm_set_component(shape, hs - y, hs - x, channel, 255);
+              }
+
+            if (d < 0)
+              d = d + (4 * x) + 6;
+            else {
+              d = d + 4 * (x - y) + 10;
+              y--;
+            }
+            x++;
+          }
+          printf("%d", rpart);
+        rpart--;
   }
+   
 }
 
 pnm
