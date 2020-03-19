6f2b50e5dea38cd06543e8747dd5404459af81d4
Marc <m21.cerutti@gmail.com>
2020-03-19 17:55:53 +0100

    convolution morphology

--- im-proc/morphology/morphology.c
+++ im-proc/morphology/morphology.c
@@ -3,55 +3,62 @@
 #include <morphology.h>
 #include <se.h>
 
-void
-maximum(unsigned short *val, unsigned short *max)
+void maximum(unsigned short *val, unsigned short *max)
 {
-  if (*val > *max) { *val = *max; }
+  if (*val > *max)
+  {
+    *val = *max;
+  }
 }
 
-void
-minimum(unsigned short *val, unsigned short *min)
+void minimum(unsigned short *val, unsigned short *min)
 {
-  if (*val < *min) { *val = *min; }
+  if (*val < *min)
+  {
+    *val = *min;
+  }
 }
 
-void
-process(int s,
-	int hs,
-	pnm ims,
-	pnm imd,
-	void (*pf)(unsigned short*, unsigned short*))
+void process(int s,
+             int hs,
+             pnm ims,
+             pnm imd,
+             void (*pf)(unsigned short *, unsigned short *))
 {
   pnm shape = se(s, hs);
 
   int imsRows = pnm_get_height(ims);
   int imsCols = pnm_get_width(ims);
 
-  for (int i = 0; i < imsRows; i++) {
-    for (int j = 0; j < imsCols; j++) {
+  for (int i = 0; i < imsRows; i++)
+  {
+    for (int j = 0; j < imsCols; j++)
+    {
 
       unsigned short res = pnm_get_component(ims, i, j, 0);
 
-      for (int x = 0; x < hs; x++) {
-        for (int y = 0; y < hs; y++) {
+      for (int x = -hs; x < hs+1; x++)
+      {
+        for (int y = -hs; y < hs+1; y++)
+        {
 
-          if ((i + x) >= imsRows || (j + y) >= imsCols) {
+          if ((i + y) >= imsRows || (j + x) >= imsCols || (i + y) < 0 || (j + x) < 0)
+          {
             continue;
           }
 
-          if (pnm_get_component(shape, x, y, 0) != 255)
+          if (pnm_get_component(shape, y + hs, x + hs, 0) != 255)
             continue;
 
-
-          unsigned short val = pnm_get_component(ims, i + x, j + y, 0);
+          unsigned short val = pnm_get_component(ims, i + y, j + x, 0);
           pf(&res, &val);
         }
       }
 
-      for (int channel = 0; channel < 3; channel++) {
+      for (int channel = 0; channel < 3; channel++)
+      {
         pnm_set_component(imd, i, j, channel, res);
       }
-
     }
   }
 
