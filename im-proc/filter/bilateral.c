f09a5d9c705800d9348900e67cf3d914ca6b165e
Marc <m21.cerutti@gmail.com>
2020-04-08 17:35:17 +0200

    bilatateral (dont work)

--- im-proc/filter/bilateral.c
+++ im-proc/filter/bilateral.c
@@ -4,58 +4,73 @@
 #include <bcl.h>
 #include <math.h>
 
+#define CORE_HSIZE 1
+#define NB_CORE ((2 * CORE_HSIZE + 1) * (2 * CORE_HSIZE + 1))
+
 //////////////////////////////////////
 // Utilities fonctions
-#define Gaussian(sigma, k) exp(-((k)*(k))/(2.0*(sigma)*(sigma)))
 
-int sortComparefunc (const void * a, const void * b) {
-   return ( *(int*)a - *(int*)b );
-}
-
-int
-median(int* V, int size)
+double Gaussian(int sigma, double k)
 {
-  qsort(V, size, sizeof(int), sortComparefunc);
-  return V[size/2];
+  return (exp(-((k) * (k)) / (2.0 * (sigma) * (sigma))));
 }
 
-void getNeighboor(int ip, int jp, int halfsize, pnm source, int rows, int cols, int *V, int *nbNeighboor)
+void indexToPosition(int index, int *i, int *j, const int cols)
+{
+  *i = index / cols;
+  *j = index % cols;
+}
+
+int positionToIndex(int i, int j, const int cols)
+{
+  return i * cols + j;
+}
+
+//////////////////////////////////////
+void getNeighboor(int i, int j, int halfsize, int rows, int cols, int *V, int *nbNeighboor)
 {
   int cpt = 0;
 
-  for (int i = ip - halfsize; i <= (ip + halfsize); i++)
+  for (int x = -halfsize; x <= halfsize; x++)
   {
-    for (int j = jp - halfsize; j <= (jp + halfsize); j++)
+    for (int y = -halfsize; y <= halfsize; y++)
     {
-      //Ignore border
-      if(i < 0  ||  i > rows || j <0 || j > cols)
+      //Border ignored
+      if ((i + y) >= rows || (i + y) < 0 || (j + x) >= cols || (j + x) < 0)
+      {
         continue;
-
-      V[cpt] = pnm_get_component(source, i, j, 0);
+      }
+      int index = positionToIndex(i + y, j + x, cols);
+      V[cpt] = index;
       cpt++;
     }
   }
   *nbNeighboor = cpt;
 }
 
-int
-bilateral(int sigma_s,int sigma_g, int* V, int cpt, int p)
+unsigned short bilateral(pnm source, int i, int j, int cols, int sigma_s, int sigma_g, int *V, int cpt)
 {
-  int up = 0;
-  int down = 0;
-  int common_factor = 0;
-  int q = 0;
-  for (int i = 0; i < cpt; i++) {
-    q = V[i];
-    common_factor = Gaussian(sigma_s, p-q)*Gaussian(sigma_g, p-q);
-    up += common_factor * q;
+  double up = 0;
+  double down = 0;
+
+  int p = positionToIndex(i, j, cols);
+  unsigned short pixel_p = pnm_get_component(source, i, j, 0);
+
+  for (int i = 0; i < cpt; i++)
+  {
+    int q = V[i];
+    int iq, jq;
+    indexToPosition(q, &iq, &jq, cols);
+    unsigned short pixel_q = pnm_get_component(source, iq, jq, 0);
+
+    double common_factor = Gaussian(sigma_s, abs(p - q)) * Gaussian(sigma_g, abs(pixel_p - pixel_q));
+    up += common_factor * pixel_q;
     down += common_factor;
   }
-  return up/down;
+  return (unsigned short)(up / down);
 }
 
-void
-process(int sigma_s, int sigma_g, char *ims, char *imd)
+void process(int sigma_s, int sigma_g, char *ims, char *imd)
 {
   pnm input = pnm_load(ims);
 
@@ -64,20 +79,15 @@ process(int sigma_s, int sigma_g, char *ims, char *imd)
 
   pnm output = pnm_new(imsRows, imsCols, PnmRawPpm);
 
-  int nbNeighboor = 0;
-  int res;
-  int halfsize = 1;
-  int size = (halfsize+1)*(halfsize+1);
-  int V[size*size];
-  int p_value;
-
   for (int i = 0; i < imsRows; i++)
   {
     for (int j = 0; j < imsCols; j++)
     {
-      p_value = pnm_get_component(input, i, j, 0);
-      getNeighboor(i, j , halfsize, input, imsRows, imsCols, V, &nbNeighboor);
-      res = bilateral(sigma_s, sigma_g, V, nbNeighboor, p_value);
+      int V[NB_CORE];
+      int nbNeighboor = 0;
+      getNeighboor(i, j, CORE_HSIZE, imsRows, imsCols, V, &nbNeighboor);
+      unsigned short res = bilateral(input, i, j, imsCols, sigma_s, sigma_g, V, nbNeighboor);
+
       for (int channel = 0; channel < 3; channel++)
         pnm_set_component(output, i, j, channel, res);
     }
@@ -86,16 +96,17 @@ process(int sigma_s, int sigma_g, char *ims, char *imd)
   pnm_save(output, PnmRawPpm, imd);
 }
 
-void
-usage (char *s){
+void usage(char *s)
+{
   fprintf(stderr, "Usage: %s <sigma_s> <sigma_g> <ims> <imd>\n", s);
   exit(EXIT_FAILURE);
 }
 
 #define PARAM 4
-int
-main(int argc, char *argv[]){
-  if (argc != PARAM+1) usage(argv[0]);
+int main(int argc, char *argv[])
+{
+  if (argc != PARAM + 1)
+    usage(argv[0]);
   process(atoi(argv[1]), atoi(argv[2]), argv[3], argv[4]);
   return EXIT_SUCCESS;
 }
